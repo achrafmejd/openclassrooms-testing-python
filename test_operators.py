ab5ad30801ca7e9133a2a971b6a9f8d1b808ef84
theM3JD <achraf.mejd.dev@gmail.com>
2022-10-07 14:43:36 +0100

    Testing Opearations with non digits and Division by 0

--- test_operators.py
+++ test_operators.py
@@ -21,3 +21,44 @@ def test_simple_division():
     operation = "5 / 1"
     expected_value = 5
     assert module_to_be_tested.division(operation) == expected_value
+
+def test_multiple_addition():
+    operation = "5 + 8 + 7"
+    expected_value = 20
+    assert module_to_be_tested.addition(operation) == expected_value
+
+def test_multiple_multiplcation():
+    operation = "4*8*2"
+    expected_value = 64
+    assert module_to_be_tested.multiplication(operation) == expected_value
+
+def test_multiple_substraction():
+    operation = "5 - 1 - 2"
+    expected_value = 2
+    assert module_to_be_tested.substraction(operation) == expected_value
+
+def test_multiple_division():
+    operation = "5 / 1 / 2"
+    expected_value = 2.5
+    assert module_to_be_tested.division(operation) == expected_value
+
+def test_division_by_0():
+    operation = "5 / 0"
+    expected_value = None
+    assert module_to_be_tested.division(operation) == expected_value
+
+
+def test_multiplcation_by_non_digit():
+    operation = "5 * A"
+    expected_value = None
+    assert module_to_be_tested.multiplication(operation) == expected_value
+
+def test_addition_by_non_digit():
+    operation = "5 + A"
+    expected_value = None
+    assert module_to_be_tested.addition(operation) == expected_value
+
+def test_substraction_by_non_digit():
+    operation = "5 - A"
+    expected_value = None
+    assert module_to_be_tested.substraction(operation) == expected_value
